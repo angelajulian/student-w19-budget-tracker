8347d8bf723e2302a3c53bee31035d0f590e9780
Julian <angela.julian@gmail.com>
2021-11-08 17:31:24 -0800

    listen I don't even know how I made that mistake but I hope I rectified it

--- public/js/idb.js
+++ public/js/idb.js
@@ -38,37 +38,37 @@ function uploadRecord() {
   const budgetObjectStore = transaction.objectStore("new_budget");
 
   const getAll = budgetObjectStore.getAll();
-}
 
-getAll.onsuccess = function () {
-  // if there was data in indexedDb's store, let's send it to the api server
-  if (getAll.result.length > 0) {
-    fetch("/api/transaction", {
-      method: "POST",
-      body: JSON.stringify(getAll.result),
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((serverResponse) => {
-        if (serverResponse.message) {
-          throw new Error(serverResponse);
-        }
-        // open one more transaction
-        const transaction = db.transaction(["new_budget"], "readwrite");
-
-        const budgetObjectStore = transaction.objectStore("new_budget");
-        // clear all items in your store
-        budgetObjectStore.clear();
-
-        alert("All transactions uploaded!");
+  getAll.onsuccess = function () {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
       })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-};
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(["new_budget"], "readwrite");
+
+          const budgetObjectStore = transaction.objectStore("new_budget");
+          // clear all items in your store
+          budgetObjectStore.clear();
+
+          alert("All transactions uploaded!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
 
 window.addEventListener("online", uploadBudget);
